70f0355ad34a08c9abf37f9c69e7792f5aec555f
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-10 13:06:36 -0500

    boiler plate code

---
+++ employeeDB.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL               
+
+)
+
+CREATE TABLE role (
+    department_id INT NOT NULL UNSIGNED,
+    Index dep_id (department_id),
+    title VARCHAR(30),
+    salary INT NOT NULL UNSIGNED
+)
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL UNSIGNED,
+    Index role_id (role_id), 
+    manager_id INT UNSIGNED,
+    Index man_id (manager_id)
+
+    -- foreign key references cascade (line14, 24 & 26)---- LOOK UP -- something along the lines of tying all the tables together
+    -- will also have to do this in the role table as well
+)
+
