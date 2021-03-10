fb8d5d40f9a61c051bf74f39e268e8a083a3cc1a
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-10 14:30:52 -0500

    progress

--- connection.js
+++ connection.js
@@ -15,90 +15,109 @@ function runSearch() {
         name: 'userChoice',
         message: 'What would you like to search?',
         choices: [
-            "Department",
-            "Role",
-            "Employee",
+            "Add a department, role, or employee",
+            "View a department, role, or employee",
+            "Update a department, role, or employee",
         ]
     }]).then((answer) => {
+        // Create a switch case for what the user wants to do with the database
         switch (answer.userChoice) {
             case "Department":
-                queryArtist();
+                queryAdd();
                 break;
             case "Role":
-                queryMultiFilter();
+                queryView();
                 break;
             case "Employee":
-                queryRange();
+                queryUpdate();
                 break;
         }
     });
 }​
-function queryArtist() {
+
+
+function queryAdd() {
     inquirer.prompt({
-            type: 'input',
-            name: 'artist',
-            message: 'What artist would you like to search for?'
+            type: 'list',
+            name: 'userAdd',
+            message: 'What would you like to add to the database?',
+            choices: [
+                "Add a department",
+                "Add an role",
+                "Add an employee",
+            ]
         })
-        .then(function (answer) {
-            const query = "SELECT artist, position, song, year FROM top5000 WHERE artist = ?";
-            connection.query(query, answer.artist, (err, results) => {
-                if (err) throw err;
-                console.log(results);
-                runSearch();
-            });
+        .then((answer) => {
+            // Create a switch case for what the user wants to add
+            switch (answer.userChoice) {
+                case "Department":
+                    let query = 'INSERT INTO Departments (name) VALUES (${})';
+                    connection.query(query, (err, results) => {
+                        if (err) throw err;
+                        console.log(results);
+                        runSearch();
+                    });
+                    break;
+                case "Role":
+                    queryAddRole();
+                    break;
+                case "Employee":
+                    queryAddEmployee();
+                    break;
+            }
         })
+
 }​
-function queryMultiFilter() {
-    const query = "SELECT artist, COUNT(*) FROM top5000 GROUP BY artist HAVING COUNT(*) > 10";
-    connection.query(query, (err, results) => {
-        if (err) throw err;
-        console.log(results);
-        runSearch();
-    });
-}​
-function queryRange() {
-    inquirer.prompt([{
-                type: 'input',
-                name: 'start',
-                message: 'What is the starting position you would like to designate?'
-            },
-            {
-                type: 'input',
-                name: 'ending',
-                message: 'What is the ending position you would like to designate?',
-                validate: (val) => {
-                    if (isNaN(val) === false) return true;
-                    return false;
-                }
-            },
-        ])
-        .then(function (answer) {
-            const query = "SELECT position, song, artist, year FROM top5000 WHERE position between ? and ?";
-            connection.query(query, [answer.start, answer.ending], (err, results) => {
-                if (err) throw err;
-                console.log(results);
-                runSearch();
-            });
-        })
-}​
-function querySongSpecific() {
+
+
+function queryView() {
     inquirer.prompt({
-            type: 'input',
-            name: 'song',
-            message: 'What song would you like to search for?'
+            type: 'list',
+            name: 'userView',
+            message: 'What would you like to view from the database?',
+            choices: [
+                "View a department",
+                "View a role",
+                "View an employee",
+            ]
         })
-        .then(function (answer) {
-            const query = "SELECT artist, position, song, year FROM top5000 WHERE ?";
-            const ret = connection.query(query, {
-                song: answer.song
-            }, (err, results) => {
-                if (err) throw err;
-                console.log(results);
-                runSearch();
-            });
-            console.log(ret.sql);
-        })
+        .then((answer) => {
+            switch (answer.userView) {
+                case "View a department":
+                    let query = 'SELECT * FROM Departments';
+                    connection.query(query, (err, results) => {
+                        if (err) throw err;
+                        console.log(results);
+                        runSearch();
+                    });
+                    break;
+                case "View a role":
+                    let query = 'SELECT * FROM role';
+                    connection.query(query, (err, results) => {
+                        if (err) throw err;
+                        console.log(results);
+                        runSearch();
+                    });
+
+                    break;
+                case "Employee":
+                    queryAddEmployee();
+                    break;
+            }
+
+
+        });
+
+    // connection.query(query, (err, results) => {
+    //     if (err) throw err;
+    //     console.log(results);
+    //     runSearch();
+
+
+
+
 }​
+
 
 
 connection.connect((err) => {
@@ -106,30 +125,6 @@ connection.connect((err) => {
     console.log(`connected as id ${connection.threadId}`);
     runSearch();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 connection.connect();
 connection.query = util.promisify(connection.query);
 
