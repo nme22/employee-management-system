01e058a82d40369cf1b054534f3b29d72d3eab3b
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-03-10 13:33:16 -0500

    got database to work in mysql

--- connection.js
+++ connection.js
@@ -9,6 +9,127 @@ const connection = mysql.createConnection({
     database: 'employeeDB'
 });
 
+function runSearch() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'userChoice',
+        message: 'What would you like to search?',
+        choices: [
+            "Department",
+            "Role",
+            "Employee",
+        ]
+    }]).then((answer) => {
+        switch (answer.userChoice) {
+            case "Department":
+                queryArtist();
+                break;
+            case "Role":
+                queryMultiFilter();
+                break;
+            case "Employee":
+                queryRange();
+                break;
+        }
+    });
+}​
+function queryArtist() {
+    inquirer.prompt({
+            type: 'input',
+            name: 'artist',
+            message: 'What artist would you like to search for?'
+        })
+        .then(function (answer) {
+            const query = "SELECT artist, position, song, year FROM top5000 WHERE artist = ?";
+            connection.query(query, answer.artist, (err, results) => {
+                if (err) throw err;
+                console.log(results);
+                runSearch();
+            });
+        })
+}​
+function queryMultiFilter() {
+    const query = "SELECT artist, COUNT(*) FROM top5000 GROUP BY artist HAVING COUNT(*) > 10";
+    connection.query(query, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        runSearch();
+    });
+}​
+function queryRange() {
+    inquirer.prompt([{
+                type: 'input',
+                name: 'start',
+                message: 'What is the starting position you would like to designate?'
+            },
+            {
+                type: 'input',
+                name: 'ending',
+                message: 'What is the ending position you would like to designate?',
+                validate: (val) => {
+                    if (isNaN(val) === false) return true;
+                    return false;
+                }
+            },
+        ])
+        .then(function (answer) {
+            const query = "SELECT position, song, artist, year FROM top5000 WHERE position between ? and ?";
+            connection.query(query, [answer.start, answer.ending], (err, results) => {
+                if (err) throw err;
+                console.log(results);
+                runSearch();
+            });
+        })
+}​
+function querySongSpecific() {
+    inquirer.prompt({
+            type: 'input',
+            name: 'song',
+            message: 'What song would you like to search for?'
+        })
+        .then(function (answer) {
+            const query = "SELECT artist, position, song, year FROM top5000 WHERE ?";
+            const ret = connection.query(query, {
+                song: answer.song
+            }, (err, results) => {
+                if (err) throw err;
+                console.log(results);
+                runSearch();
+            });
+            console.log(ret.sql);
+        })
+}​
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    runSearch();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 connection.connect();
 connection.query = util.promisify(connection.query);
 
--- db/employeeDB.sql
+++ db/employeeDB.sql
@@ -7,25 +7,26 @@ CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL               
 
-)
+);
 
 CREATE TABLE role (
-    department_id INT NOT NULL UNSIGNED,
-    Index dep_id (department_id),
-    title VARCHAR(30),
-    salary INT NOT NULL UNSIGNED
-)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL ,
+    salary INT UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    Index dep_id (department_id)
+);
 
 CREATE TABLE employee (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL UNSIGNED,
+    role_id INT UNSIGNED NOT NULL ,
     Index role_id (role_id), 
     manager_id INT UNSIGNED,
     Index man_id (manager_id)
 
     -- foreign key references cascade (line14, 24 & 26)---- LOOK UP -- something along the lines of tying all the tables together
     -- will also have to do this in the role table as well
-)
+);
 
